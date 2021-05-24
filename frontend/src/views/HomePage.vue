68aaeb3d3ac95fc65ea5d8bac7841eea4d7b0982
Matthias Holcombe <matthiash@localhost.localdomain>
2021-05-24 17:15:53 -0500

    first

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home-page">
+    <div class="home-page-banner">
+      Welcome to Pair Stairs dot something, you ready to pair up some stairs??????
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomePage"
+});
+</script>
+
+<style lang="scss">
+@import "../colors.scss";
+
+.home-page {    
+    @include color-theme('background-color', 'primary-background');
+
+    padding-top: 150px;
+    height: 100%;
+    width: 100%;
+}
+
+.home-page-banner {
+  @include color-theme('color', 'primary-accent');
+
+  width: 100%;
+  margin-top: 150px;
+}
+</style>
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+let corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'));
+
+app.use("/", express.static("frontend/dist"));
+app.get("/*", (req, res) => {
+    const path = require("path");
+    res.sendFile(path.resolve("frontend/dist/index.html"));
+})
+
+// app.get("/", (req, res) => {
+//     res.json({ message: "Welcome" })
+// });
+
+const PORT = process.env.PORT ||8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
